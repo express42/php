179de341278f39d02bfa804b004c95312fd30b38
Nikita Borzykh <sample.n@gmail.com>
2013-08-31 22:40:54 +0400

    fix type

--- resources/pool.rb
+++ resources/pool.rb
@@ -8,6 +8,6 @@ attribute :allow, :kind_of => String, :default => "127.0.0.1"
 attribute :address, :kind_of => String, :default => "127.0.0.1"
 attribute :port, :kind_of => String
 attribute :backlog, :kind_of => Integer, :default => -1
-attribute :request_terminate_timeout, :kind_of => Integer, :default => "60s"
+attribute :request_terminate_timeout, :kind_of => [Integer, String], :default => "60s"
 attribute :limits, :kind_of => Hash, :default => {:core => 0, :files => 1024, :requests => 500, :children => 5, :spare_children => { :min => 1, :max => 3 } }
 attribute :php_var, :kind_of => Hash, :default => {}
