e2155861d970ea6576b124040751dcc51ad8bbbc
Alexander Titov <titoff.a@gmail.com>
2013-11-01 15:27:51 +0400

    cleanup for unnecessary

--- providers/pool.rb
+++ providers/pool.rb
@@ -1,5 +1,41 @@
-action :add do
+def load_current_resource
+  @current_resource = Chef::Resource::PhpPool.new(@new_resource.name)
+
+  pool_path = node[:php][:fpm][:pools_dir]
+
+  # get list of files from conf dir
+  files_list =[]
+  Dir.glob(pool_path + "/*").each { |path| files_list.push(path.sub(pool_path + "/", '')) }
+
+  resources_list = []
   
+  # get file names from resource_collection
+  run_context.resource_collection.each do |resource|
+    if resource.class == Chef::Resource::PhpPool
+      resources_list.push(resource.name + ".conf")
+    end
+  end
+    
+  resource_files = []
+  
+  # get list of files created by provider
+  resources_list.each do |r|
+    files_list.each do |f|
+      if f.match(r) 
+        resource_files.push(f)
+      end
+    end
+  end
+  
+  # delete unnecessary files
+  (files_list - resource_files).each do |f|
+    ::File::unlink(pool_path + "/" +f)
+    Chef::Log.info("Removing file " + f + " in directory " + pool_path + " ...")
+  end
+  @current_resource
+end
+
+action :add do
   template ::File.join(node[:php][:fpm][:pools_dir], new_resource.name + ".conf") do
     mode "0644"
     source "fpm-pool.conf.erb"
