2da9bb6e8c7fa68821d971e89f4476f2acc2ded5
Igor Kurochkin <ikurochkin@express42.com>
2014-11-11 17:03:25 +0300

    Rubocop and Foodcritic

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default[:php][:fpm][:pools_dir] = "/etc/php5/fpm/pool.d"
-default[:php][:fpm][:main_conf] = "/etc/php5/fpm/php-fpm.conf"
-default[:php][:fpm][:create_default_pool] = true
+default['php']['fpm']['pools_dir'] = '/etc/php5/fpm/pool.d'
+default['php']['fpm']['main_conf'] = '/etc/php5/fpm/php-fpm.conf'
+default['php']['fpm']['create_default_pool'] = true
--- providers/pool.rb
+++ providers/pool.rb
@@ -1,44 +1,44 @@
 def load_current_resource
   @current_resource = Chef::Resource::PhpPool.new(@new_resource.name)
 
-  pool_path = node[:php][:fpm][:pools_dir]
+  pool_path = node['php']['fpm']['pools_dir']
 
   # get list of files from conf dir
-  files_list =[]
-  Dir.glob(pool_path + "/*").each { |path| files_list.push(path.sub(pool_path + "/", '')) }
+  files_list = []
+  Dir.glob(pool_path + '/*').each { |path| files_list.push(path.sub(pool_path + '/', '')) }
 
   resources_list = []
-  
+
   # get file names from resource_collection
   run_context.resource_collection.each do |resource|
     if resource.class == Chef::Resource::PhpPool
-      resources_list.push(resource.name + ".conf")
+      resources_list.push(resource.name + '.conf')
     end
   end
-    
+
   resource_files = []
-  
+
   # get list of files created by provider
   resources_list.each do |r|
     files_list.each do |f|
-      if f.match(r) 
+      if f.match(r)
         resource_files.push(f)
       end
     end
   end
-  
+
   # delete unnecessary files
   (files_list - resource_files).each do |f|
-    ::File::unlink(pool_path + "/" +f)
-    Chef::Log.info("Removing file " + f + " in directory " + pool_path + " ...")
+    ::File::unlink(pool_path + '/' + f)
+    Chef::Log.info('Removing file ' + f + ' in directory ' + pool_path + ' ...')
   end
   @current_resource
 end
 
 action :add do
-  template ::File.join(node[:php][:fpm][:pools_dir], new_resource.name + ".conf") do
-    mode "0644"
-    source "fpm-pool.conf.erb"
+  template ::File.join(node['php']['fpm']['pools_dir'], new_resource.name + '.conf') do
+    mode '0644'
+    source 'fpm-pool.conf.erb'
     variables(
       :name => new_resource.name,
       :user => new_resource.user,
@@ -52,7 +52,7 @@ action :add do
       :php_user => new_resource.php_user_var,
       :request_terminate_timeout => new_resource.request_terminate_timeout
     )
-    notifies :reload, resources(:service => "php5-fpm")
-    cookbook "php"
+    notifies :reload, 'service[php5-fpm]'
+    cookbook 'php'
   end
 end
