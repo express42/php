87c53650466df40b1168cd6d71a853e754e4fc7d
Igor Kurochkin <ikurochkin@express42.com>
2014-11-11 16:56:52 +0300

    Add upstart script

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,34 +7,40 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w{php5-gd php5-fpm php5-cli php5-common}.each do |pkg|
+%w(php5-gd php5-fpm php5-cli php5-common).each do |pkg|
   package pkg do
     action :install
   end
 end
 
 if node['platform_version'] == '14.04'
-  service "php5-fpm" do
-    supports :status => true, :restart => true
-    action [ :enable, :start ]
+  cookbook_file '/etc/init/php5-fpm.conf' do
+    source 'php5-fpm.conf'
+    owner 'root'
+    group 'root'
+    mode '0644'
+  end
+  service 'php5-fpm' do
+    supports status: true, restart: true
+    action [:enable, :start]
     provider Chef::Provider::Service::Upstart
   end
 else
-  service "php5-fpm" do
-    supports :status => true, :restart => true, :reload => true
-    action [ :enable, :start ]
+  service 'php5-fpm' do
+    supports status: true, restart: true, reload: true
+    action [:enable, :start]
   end
 end
 
-template node[:php][:fpm][:main_conf] do
-  source "fpm-main.conf.erb"
-  mode 0644
-  notifies :restart, resources(:service => "php5-fpm"), :immediately
+template node['php']['fpm']['main_conf'] do
+  source 'fpm-main.conf.erb'
+  mode '0644'
+  notifies :restart, 'service[php5-fpm]', :immediately
 end
 
 if node['php']['fpm']['create_default_pool']
-  php_pool "www" do
-    port "9000"
+  php_pool 'www' do
+    port '9000'
     action :add
   end
 end
