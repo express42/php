39cc4329241ac30387098bffc3fd164cd48f3fee
Igor Kurochkin <ikurochkin@express42.com>
2014-12-03 19:53:19 +0300

    Version bump to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,10 @@ name             'php'
 maintainer       'LLC Express 42'
 maintainer_email 'cookbooks@express42.com'
 license          'MIT'
-description      'Installs and configures php-fpm'
+description      'Installs and configures php-fpm. Provides LWRPs for managing multiple instances.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
+
+recipe           'php::default', 'Installs and configures default php-fpm instance'
 
 supports         'ubuntu'
